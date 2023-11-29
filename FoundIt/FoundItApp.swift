d3d9aa6eff9cdef6d72fd3ca4842713f51ebdf37
Tanya Aravind <ta374@cornell.edu>
2023-11-29 14:21:50 -0500

    XCode Files Added

---
+++ FoundIt/FoundItApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundItApp.swift
+//  FoundIt
+//
+//  Created by Tanya Aravind on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FoundItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
