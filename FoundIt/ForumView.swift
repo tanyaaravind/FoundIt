b54745a44b4912859c201a0070b41beaadf35dea
Tanya Aravind <ta374@cornell.edu>
2023-11-30 00:48:04 -0500

    Nav bar and toggle buttons, all commits

--- FoundIt/ForumView.swift
+++ FoundIt/ForumView.swift
@@ -7,4 +7,75 @@
 
 import SwiftUI
 
+struct ForumView: View {
+    @State private var searchBar: String = ""
+    
+    enum ButtonType {
+           case Lost, Found
+       }
+    
+    @State private var selectedButton: ButtonType = .Lost
 
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    TextField("Search", text: $searchBar)
+                        .padding(.vertical, 10)
+                }
+                .padding()
+                .background(Color.gray)
+                .cornerRadius(36)
+                .padding(.top, 15)
+                .padding(.trailing, 10)
+                .padding(.leading, 10)
+                
+                VStack {
+                    HStack {
+                        Spacer()
+                        Button("Lost") {
+                            self.selectedButton = .Lost
+                        }
+                        .padding()
+                        .frame(width: 90)
+                        .background(selectedButton == .Lost ? Color.blue : Color.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        Spacer()
+                        
+                        Spacer()
+                        Button("Found") {
+                            self.selectedButton = .Found
+                        }
+                        .padding()
+                        .frame(width: 90)
+                        .background(selectedButton == .Found ? Color.blue : Color.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        Spacer()
+                    }
+                    .padding()
+                    
+                    // Display data based on the selected button
+                    if selectedButton == .Lost {
+                        Text("Lost")
+                            .padding()
+                    } else {
+                        Text("Found")
+                            .padding()
+                    }
+                    
+                    Spacer()
+                }
+                .navigationTitle("Recover")
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ForumView()
+}
--- FoundIt/NavBar.swift
+++ FoundIt/NavBar.swift
@@ -5,4 +5,36 @@
 //  Created by Tanya Aravind on 11/30/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct NavBar: View {
+    var body: some View {
+        TabView {
+            // Home Tab
+            ForumView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            
+            // Schedule Tab
+            Text("Post")
+                .tabItem {
+                    Image(systemName: "plus.circle.fill")
+                    Text("Post")
+                }
+            
+            // Locations Tab
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person.circle.fill")
+                    Text("Profile")
+                }
+        }
+    }
+}
+
+
+#Preview {
+    NavBar()
+}
